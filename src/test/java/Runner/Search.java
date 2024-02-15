a6fb68e88e352122d63c762e63688a129f5349d2
Miruthula <miruthulas12@gmail.com>
2024-02-15 16:51:13 +0530

    added cucumber test

---
+++ src/test/java/Runner/Search.java
@@ -0,0 +1,21 @@
+package Runner;
+
+
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = {"stepdefinition"},
+    tags = "@tag",
+    plugin = {"html:target/html-report",
+            "json:target/json-report/cucumber.json",
+            "junit:target/junit-report/cucumber.xml",
+            "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:target/html/ExtentReport.html"}
+    
+)
+public class Search {
+}
---
+++ src/test/java/stepdefinition/SharedSteps.java
@@ -0,0 +1,30 @@
+package stepdefinition;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import io.cucumber.java.Status;
+
+public class SharedSteps {
+	private WebDriver driver;
+
+    @Before
+    public void setup(Scenario scenario) {
+        // Set up WebDriver
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\AlphaMedias\\eclipse-workspace\\CucumberTest\\drivers\\windows\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void tearDown() {
+        //Browser close
+        driver.quit();
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+}
